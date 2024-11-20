c4fab2bec9d1d13c9452cabbfc58cc823f2ab1e4
Fabian Flores <favian.flone92@gmail.com>
2024-11-20 17:07:24 -0600

    Implement status management for articles and comments with visibility controls

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
   def index
-    @articles = Article.all
+    @articles = Article.list_public
   end
 
   def show
@@ -44,6 +46,6 @@ class ArticlesController < ApplicationController
 
   private
     def article_params
-      params.expect(article: [:title, :body])
+      params.expect(article: [:title, :body, :status])
     end
 end
